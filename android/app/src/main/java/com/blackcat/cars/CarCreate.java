0b08f0fe2b9f2bc77360a26a03426667ae90de76
siqqQ <b.bohosyan@gmail.com>
2018-09-05 17:18:57 +0300

    Merge server branch with master and added android initial commmit

---
+++ android/app/src/main/java/com/blackcat/cars/CarCreate.java
@@ -0,0 +1,13 @@
+package com.blackcat.cars;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarCreate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_create);
+    }
+}
