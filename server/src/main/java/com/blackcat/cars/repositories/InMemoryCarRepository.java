684fb783eee073e6b1263a3f82d7396ff9cb466c
siqqQ <b.bohosyan@gmail.com>
2018-09-05 16:10:26 +0300

    Implement methods from CarRepository in InMemoryCarRepository

--- server/src/main/java/com/blackcat/cars/repositories/InMemoryCarRepository.java
+++ server/src/main/java/com/blackcat/cars/repositories/InMemoryCarRepository.java
@@ -1,4 +1,32 @@
 package com.blackcat.cars.repositories;
 
+import com.blackcat.cars.models.Car;
+
+import java.util.List;
+
 public class InMemoryCarRepository implements CarRepository {
+    @Override
+    public void createCar(Car car) {
+
+    }
+
+    @Override
+    public List<Car> getAllCars() {
+        return null;
+    }
+
+    @Override
+    public Car getCarById(int id) {
+        return null;
+    }
+
+    @Override
+    public void updateCar(int id, Car car) {
+
+    }
+
+    @Override
+    public void deleteCar(int id) {
+
+    }
 }
