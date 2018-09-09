080bbe0e663ea23c00cd5926ac30227669fd3adf
siqqQ <b.bohosyan@gmail.com>
2018-09-09 18:30:27 +0300

    Implemented CarCreatePresenter.java

--- android/app/src/main/java/com/blackcat/cars/views/CarCreate/CarCreatePresenter.java
+++ android/app/src/main/java/com/blackcat/cars/views/CarCreate/CarCreatePresenter.java
@@ -1,4 +1,51 @@
 package com.blackcat.cars.views.CarCreate;
 
-public class CarCreatePresenter {
+import com.blackcat.cars.models.Car;
+
+import javax.inject.Inject;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.disposables.Disposable;
+
+public class CarCreatePresenter implements  CarCreateContracts.Presenter{
+
+    private final CarsService mCarsService;
+    private final SchedulerProvider mSchedulerProvider;
+    private CarCreateContracts.View mView;
+
+    @Inject
+    public SuperheroCreatePresenter(
+            CarsService carsService,
+            SchedulerProvider schedulerProvider) {
+        mCarsService = carsService;
+        mSchedulerProvider = schedulerProvider;
+    }
+
+    @Override
+    public void subscribe(CarCreateContracts.View view) {
+        mView = view;
+    }
+
+    @Override
+    public void unsubscribe() {
+        mView = null;
+    }
+
+    @Override
+    public void save(Car car) {
+        mView.showLoading();
+        Disposable disposable = Observable
+                .create((ObservableOnSubscribe<Car>) emitter -> {
+                    Car createdCar = mCarsService.createCar(car);
+                    emitter.onNext(createdCar);
+                    emitter.onComplete();
+                })
+                .subscribeOn(mSchedulerProvider.background())
+                .observeOn(mSchedulerProvider.ui())
+                .doOnEach(x -> mView.hideLoading())
+                .doOnError(mView::showError)
+                .subscribe(s -> mView.navigateToHome());
+    }
+
 }
