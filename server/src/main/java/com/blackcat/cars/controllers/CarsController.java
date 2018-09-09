b690db6799c5ff9c5854a49f9e67ed8ed2c4b249
siqqQ <b.bohosyan@gmail.com>
2018-09-09 15:55:49 +0300

    Added setter and getter for service variable in CarsController and used in the constructor

--- server/src/main/java/com/blackcat/cars/controllers/CarsController.java
+++ server/src/main/java/com/blackcat/cars/controllers/CarsController.java
@@ -1,9 +1,25 @@
 package com.blackcat.cars.controllers;
 
+import com.blackcat.cars.services.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/cars")
 public class CarsController {
+    private CarService service;
+
+    @Autowired
+    public CarsController(CarService carService){
+        setCarService(carService);
+    }
+
+    public CarService getService() {
+        return service;
+    }
+
+    public void setCarService(CarService service) {
+        this.service = service;
+    }
 }
