527d9d4e62f6cd90f0876a9db999eb6f5b06dd90
siqqQ <b.bohosyan@gmail.com>
2018-09-09 17:15:49 +0300

    Added IDENTIFIER variable in CarCreteActivity

--- android/app/src/main/java/com/blackcat/cars/views/CarCreate/CarCreateActivity.java
+++ android/app/src/main/java/com/blackcat/cars/views/CarCreate/CarCreateActivity.java
@@ -7,6 +7,8 @@ import com.blackcat.cars.R;
 
 public class CarCreateActivity extends AppCompatActivity {
 
+    public static final long IDENTIFIER = 298;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
