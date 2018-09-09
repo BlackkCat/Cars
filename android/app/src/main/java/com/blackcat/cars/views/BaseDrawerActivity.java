a0901994aad2e9e6be596a0ef4f836ef43ad97d5
siqqQ <b.bohosyan@gmail.com>
2018-09-09 17:01:44 +0300

    Added Toolbar and FrameLayout in RalativeLayout in activity_car_create.xml

--- android/app/src/main/java/com/blackcat/cars/views/BaseDrawerActivity.java
+++ android/app/src/main/java/com/blackcat/cars/views/BaseDrawerActivity.java
@@ -2,12 +2,17 @@ package com.blackcat.cars.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 import com.blackcat.cars.R;
 
+import butterknife.BindView;
 import dagger.android.support.DaggerAppCompatActivity;
 
 public abstract class BaseDrawerActivity extends DaggerAppCompatActivity {
+
+    @BindView(R.id.drawer_toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
