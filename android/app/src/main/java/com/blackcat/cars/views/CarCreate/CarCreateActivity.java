c32fc5091699331d546d3c9401f7fc532db52d72
siqqQ <b.bohosyan@gmail.com>
2018-09-09 17:45:16 +0300

    Added loader of fragment in onCreate

--- android/app/src/main/java/com/blackcat/cars/views/CarCreate/CarCreateActivity.java
+++ android/app/src/main/java/com/blackcat/cars/views/CarCreate/CarCreateActivity.java
@@ -10,6 +10,8 @@ import com.blackcat.cars.views.CarsList.CarsListActivity;
 
 import javax.inject.Inject;
 
+import butterknife.ButterKnife;
+
 public class CarCreateActivity extends BaseDrawerActivity implements  CarCreateContracts.Navigator {
 
     public static final long IDENTIFIER = 298;
@@ -23,7 +25,17 @@ public class CarCreateActivity extends BaseDrawerActivity implements  CarCreateC
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_car_create);
+        setContentView(R.layout.activity_superhero_create);
+        ButterKnife.bind(this);
+
+        mView.setPresenter(mPresenter);
+        mView.setNavigator(this);
+
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content, mView)
+                .commit();
+
     }
 
     @Override
