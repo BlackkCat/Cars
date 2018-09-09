92ede174bb33a6bdfbeb5f32505097a1b8cee4b2
siqqQ <b.bohosyan@gmail.com>
2018-09-09 15:53:30 +0300

    Added CarsController.java file and set basic annotations - RestController, RequestMapping

---
+++ server/src/main/java/com/blackcat/cars/controllers/CarsController.java
@@ -0,0 +1,9 @@
+package com.blackcat.cars.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cars")
+public class CarsController {
+}
