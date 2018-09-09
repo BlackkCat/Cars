da1a4863f2b42253c8e7c3f4e2f9a08a5f3ca2b0
siqqQ <b.bohosyan@gmail.com>
2018-09-09 17:07:44 +0300

    Added method for setting drawer in BaseDrawerActivity - still not implemented all needed methods

--- android/app/src/main/java/com/blackcat/cars/views/BaseDrawerActivity.java
+++ android/app/src/main/java/com/blackcat/cars/views/BaseDrawerActivity.java
@@ -1,10 +1,18 @@
 package com.blackcat.cars.views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.blackcat.cars.R;
+import com.blackcat.cars.views.CarCreate.CarCreateActivity;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import butterknife.BindView;
 import dagger.android.support.DaggerAppCompatActivity;
@@ -15,8 +23,51 @@ public abstract class BaseDrawerActivity extends DaggerAppCompatActivity {
     Toolbar mToolbar;
 
     public BaseDrawerActivity(){
-        
+
     }
+
+    public void setupDrawer() {
+        PrimaryDrawerItem listSuperheroesItem = new PrimaryDrawerItem()
+                .withIdentifier(CarListActivity.IDENTIFIER)
+                .withName("Superheroes");
+
+        PrimaryDrawerItem createSuperheroItem = new PrimaryDrawerItem()
+                .withIdentifier(CarCreateActivity.IDENTIFIER)
+                .withIcon(android.R.drawable.btn_plus)
+                .withName("Create superhero");
+
+        Drawer drawer = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(mToolbar)
+                .addDrawerItems(
+                        listSuperheroesItem,
+                        new DividerDrawerItem(),
+                        createSuperheroItem
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(
+                            View view,
+                            int position,
+                            IDrawerItem drawerItem) {
+                        long identifier = drawerItem.getIdentifier();
+
+                        if (getIdentifier() == identifier) {
+                            return false;
+                        }
+
+                        Intent intent = getNextIntent(identifier);
+                        if (intent == null) {
+                            return false;
+                        }
+
+                        startActivity(intent);
+                        return true;
+                    }
+                })
+                .build();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
