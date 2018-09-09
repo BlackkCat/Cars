c772b4ab63af8e51a1e76fb2fea3f7e0ba3f84fa
siqqQ <b.bohosyan@gmail.com>
2018-09-09 17:42:59 +0300

    Added IDENTIFIER to CarsListActivity.java

--- android/app/src/main/java/com/blackcat/cars/views/CarsList/CarsListActivity.java
+++ android/app/src/main/java/com/blackcat/cars/views/CarsList/CarsListActivity.java
@@ -7,6 +7,8 @@ import com.blackcat.cars.R;
 
 public class CarsListActivity extends AppCompatActivity {
 
+    public static final long IDENTIFIER = 49;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
