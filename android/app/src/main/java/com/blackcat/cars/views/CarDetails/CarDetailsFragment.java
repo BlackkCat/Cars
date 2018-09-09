777959c58feedd1244caa13a58a01ce28b780b4a
siqqQ <b.bohosyan@gmail.com>
2018-09-09 17:18:33 +0300

    Added package in view folder for CarDetails as well as needed class(Activity, Contracts, Fragment, Presenter

---
+++ android/app/src/main/java/com/blackcat/cars/views/CarDetails/CarDetailsFragment.java
@@ -0,0 +1,31 @@
+package com.blackcat.cars.views.CarDetails;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.blackcat.cars.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CarDetailsFragment extends Fragment {
+
+
+    public CarDetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_car_details, container, false);
+    }
+
+}
---
+++ android/app/src/main/java/com/blackcat/cars/views/CarDetails/CarDetailsPresenter.java
@@ -0,0 +1,4 @@
+package com.blackcat.cars.views.CarDetails;
+
+public class CarDetailsPresenter {
+}
