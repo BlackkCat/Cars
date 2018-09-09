e91001b5fc7f9f4dbfd02d7a661321dfd464ef71
siqqQ <b.bohosyan@gmail.com>
2018-09-09 17:26:05 +0300

    Added all Car contracts

--- android/app/src/main/java/com/blackcat/cars/views/CarCreate/CarCreateContracts.java
+++ android/app/src/main/java/com/blackcat/cars/views/CarCreate/CarCreateContracts.java
@@ -1,4 +1,31 @@
 package com.blackcat.cars.views.CarCreate;
 
-public class CarCreateContracts {
+public interface CarCreateContracts {
+    interface View {
+
+        void setPresenter(Presenter presenter);
+
+        void navigateToHome();
+
+        void showError(Throwable throwable);
+
+        void hideLoading();
+
+        void showLoading();
+    }
+
+    interface Presenter {
+
+        void subscribe(View view);
+
+        void unsubscribe();
+
+        void save(Car car);
+    }
+
+    public interface Navigator {
+
+        void navigateToHome();
+    }
+
 }
