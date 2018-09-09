2e01db2ff4d97928934fb2d22c750af7bbb6323a
siqqQ <b.bohosyan@gmail.com>
2018-09-09 23:59:45 +0300

    Fix gradle build

--- android/app/src/main/java/com/blackcat/cars/CarApplication.java
+++ android/app/src/main/java/com/blackcat/cars/CarApplication.java
@@ -1,13 +1,14 @@
 package com.blackcat.cars;
 
+import com.blackcat.cars.diconfig.DaggerAppComponent;
+
 import dagger.android.AndroidInjector;
 import dagger.android.DaggerApplication;
 
 public class CarApplication extends DaggerApplication {
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-        return null;
-        //return DaggerAppComponent.builder().application(this).build();
+        return DaggerAppComponent.builder().application(this).build();
     }
 }
 
