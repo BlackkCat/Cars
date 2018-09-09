7461c2cb688f66ab5b2d9a2f1cbf7b4bbad17eac
siqqQ <b.bohosyan@gmail.com>
2018-09-09 19:31:03 +0300

    Overriden background and ui methods from SchedulerProvider in SyncSchedulerProvider

---
+++ android/app/src/main/java/com/blackcat/cars/async/SyncSchedulerProvider.java
@@ -0,0 +1,18 @@
+package com.blackcat.cars.async;
+
+import com.blackcat.cars.async.base.SchedulerProvider;
+
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
+
+public class SyncSchedulerProvider implements SchedulerProvider {
+    @Override
+    public Scheduler background() {
+        return Schedulers.trampoline();
+    }
+
+    @Override
+    public Scheduler ui() {
+        return Schedulers.trampoline();
+    }
+}
