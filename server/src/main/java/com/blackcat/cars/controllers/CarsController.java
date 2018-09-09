e98c14033851695954264b89ec8cc95ba6f03423
siqqQ <b.bohosyan@gmail.com>
2018-09-09 15:58:01 +0300

    Added createCar and getAllCars methods in CarsController

--- server/src/main/java/com/blackcat/cars/controllers/CarsController.java
+++ server/src/main/java/com/blackcat/cars/controllers/CarsController.java
@@ -1,9 +1,11 @@
 package com.blackcat.cars.controllers;
 
+import com.blackcat.cars.models.Car;
 import com.blackcat.cars.services.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cars")
@@ -13,6 +15,16 @@ public class CarsController {
     @Autowired
     public CarsController(CarService carService){
         setCarService(carService);
+    }
+
+    @PostMapping("/new")
+    public void createCar(@RequestBody Car car){
+        service.createCar(car);
+    }
+
+    @GetMapping
+    public List<Car> getAllCars(){
+        return service.getAllCars();
     }
 
     public CarService getService() {
